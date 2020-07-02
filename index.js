6e71c7a1195e9ad52b0a4e8dc11f4a8ba4cec32b
akshay <tvibhu12@gmail.com>
2020-07-02 15:01:20 +0530

    define two main routes

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ app.use(express.json({extend:false}))
 const PORT=5000;
 //conenect Mongo
  connectDB();
-
+//define Routes
+app.use('/',require('./routes/index'))
+app.use('/api/url',require('./routes/url'))
 
 app.listen(PORT,()=>console.log(`server runs on ${PORT}`))
